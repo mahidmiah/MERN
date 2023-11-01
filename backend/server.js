8840c59eb4b59d6784ee98a63f2f58b784ad929c
Mahid Miah <miah.mahid@outlook.com>
2023-11-01 21:00:20 +0000

    Started working on user model and api route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 
 // express app
 const app = express();
@@ -23,6 +24,7 @@ app.use((req, res, next) => {
 
 // route handlers
 app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 
 // Connect to the DB
